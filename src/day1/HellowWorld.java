41ec32f6af9005df79d1d26c956487912a72439c
aliptonga <aliptonga@gmail.com>
2021-04-25 16:35:48 -0700

    added one more line of code

--- src/day1/HellowWorld.java
+++ src/day1/HellowWorld.java
@@ -3,5 +3,6 @@ package day1;
 public class HellowWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Adding GitHub now");
     }
 }
