b2412008b904096dad7b7fae440e0507061267be
aliptonga <aliptonga@gmail.com>
2021-04-25 16:43:14 -0700

    added more work after publishing

--- src/day1/HellowWorld.java
+++ src/day1/HellowWorld.java
@@ -4,5 +4,6 @@ public class HellowWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Adding GitHub now");
+        System.out.println("yay things are on GitHub");
     }
 }
